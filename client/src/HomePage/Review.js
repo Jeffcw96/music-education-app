1da8f27a43138603189a9c84429acd6e8c9685c7
JeffCW <changcheewah96@gmail.com>
2020-12-02 23:05:36 +0800

    add chart js

--- client/src/HomePage/Review.js
+++ client/src/HomePage/Review.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import { React } from 'react'
+import { Pie } from 'react-chartjs-2';
 
 export default function Review() {
-    return (
-        <div>
 
+    const chartGap = {
+        margin: '7vh 0',
+        position: 'relative',
+        maxHeight: '700px'
+    }
+
+    const state = {
+        labels: ['Music Competition', 'Music Challenge', '1 On 1 Mentor', 'Music Editor', 'Music Sheet & Chord', 'Instructment'],
+        datasets: [{
+            data: [1, 1, 1, 1, 1, 1],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    }
+
+    return (
+        <div style={chartGap}>
+            <Pie height={700} data={state}
+                options={{
+                    cutoutPercentage: 90,
+                    maintainAspectRatio: false,
+                    animation: {
+                        duration: 2000,
+                        onComplete: function (e) {
+                            console.log("chart loaded");
+                        }
+                    },
+                    tooltips: {
+                        enabled: false
+                    },
+                    legend: {
+                        display: false
+                    },
+                }} />
+            <div className='tr'></div>
         </div>
     )
 }
--- client/src/Route/Home.js
+++ client/src/Route/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import MainPage from '../MainPage.js'
 import Plan from '../HomePage/Plans.js'
+import Review from '../HomePage/Review.js'
 import Nav from '../Nav.js'
+
 export default function Home() {
     return (
         <>
             <Nav />
             <div className="home-page-container">
                 <MainPage />
+                <Review />
                 <Plan />
             </div>
         </>
