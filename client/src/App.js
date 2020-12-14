4cb01800f23d9156841a896a69b9f35baffb0334
JeffCW <changcheewah96@gmail.com>
2020-12-14 19:22:56 +0800

    test

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,23 @@ import Plan from './Route/Plan'
 import ForgotPassword from './Route/ForgotPassword.js'
 import ResetPassword from './Route/ResetPassword.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Fragment } from 'react';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/forgotPassword" component={ForgotPassword} />
-        <Route path="/resetPassword" component={ResetPassword} />
-        <Route path="/user" component={User} />
-        <Route path="/plan/:id" component={Plan} />
-      </Switch>
+      <Fragment>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/forgotPassword" component={ForgotPassword} />
+          <Route path="/resetPassword" component={ResetPassword} />
+          <Route path="/user" component={User} />
+          <Route path="/plan/:id" component={Plan} />
+        </Switch>
+        <Footer />
+      </Fragment>
     </Router>
 
   );
